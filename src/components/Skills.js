1224b7e162b7837e935bdd873a7100b050ef03ac
hosecloud <4osecloud@gmail.com>
2023-03-03 00:35:46 +0900

    Add tech stacks

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,5 +1,58 @@
 function Skills() {
-  return <h1>Skills</h1>;
+  return (
+    <div id="Skills">
+      <div className="flex flex-wrap m-2 space-x-1">
+        <img
+          src="https://img.shields.io/badge/c-%2300599C.svg?style=for-the-badge&logo=c&logoColor=white"
+          alt="C badge"
+        />
+        <img
+          src="https://img.shields.io/badge/c++-00599C?style=for-the-badge&logo=c%2B%2B&logoColor=white"
+          alt="C++ badge"
+        />
+        <img
+          src="https://img.shields.io/badge/python-3670A0?style=for-the-badge&logo=python&logoColor=ffdd54"
+          alt="Python badge"
+        />
+        <img
+          src="https://img.shields.io/badge/java-%23ED8B00.svg?style=for-the-badge&logo=java&logoColor=white"
+          alt="Java badge"
+        />
+        <img
+          src="https://img.shields.io/badge/node.js-6DA55F?style=for-the-badge&logo=node.js&logoColor=white"
+          alt="Node.js badge"
+        />
+        <img
+          src="https://img.shields.io/badge/react-%2320232a.svg?style=for-the-badge&logo=react&logoColor=%2361DAFB"
+          alt="REACT badge"
+        />
+        <img
+          src="https://img.shields.io/badge/react_native-%2320232a.svg?style=for-the-badge&logo=react&logoColor=%2361DAFB"
+          alt="React Native badge"
+        />
+        <img
+          src="https://img.shields.io/badge/dart-%230175C2.svg?style=for-the-badge&logo=dart&logoColor=white"
+          alt="DART badge"
+        />
+        <img
+          src="https://img.shields.io/badge/Flutter-%2302569B.svg?style=for-the-badge&logo=Flutter&logoColor=white"
+          alt="FLUTTER badge"
+        />
+        <img
+          src="https://img.shields.io/badge/sqlite-%2307405e.svg?style=for-the-badge&logo=sqlite&logoColor=white"
+          alt="SQLite badge"
+        />
+        <img
+          src="https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white"
+          alt="HTML5 badge"
+        />
+        <img
+          src="https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white"
+          alt="CSS3 badge"
+        />
+      </div>
+    </div>
+  );
 }
 
 export default Skills;
